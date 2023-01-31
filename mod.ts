5175c288c1899338d801f263b80f69a42073036e
Eric Miffre <eric.miffre@gmail.com>
2023-01-31 16:07:33 +0100

    [Api] Export of default loaders (symbols and types)

--- mod.ts
+++ mod.ts
@@ -1,5 +1,23 @@
 
 export type { NodeKind } from "./sources/Graph.ts"
 
-export type { FileNode, DirectoryNode, Payload } from "./sources/Walker.ts"
-export { Walker } from "./sources/Walker.ts"
+export
+{
+	Walker,
+	type FileNode,
+	type DirectoryNode,
+	type Payload
+	
+} from "./sources/Walker.ts"
+
+export
+{
+	defaultTextLoader,
+	defaultJsonLoader,
+	defaultJavascriptLoader,
+	
+	type TextPayload,
+	type JsonPayload,
+	type JavascriptPayload,
+	
+} from "./sources/Walker.ts"
